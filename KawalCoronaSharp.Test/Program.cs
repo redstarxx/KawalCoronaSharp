3572f015796c248406c682001b2bf57b89d59ee5
RedStar <mutazzmutazz11@gmail.com>
2022-01-18 10:23:40 +0700

    more tests

--- KawalCoronaSharp.Test/Program.cs
+++ KawalCoronaSharp.Test/Program.cs
@@ -12,6 +12,14 @@ namespace KawalCoronaSharp.Test
             var response = await api.GetIndonesianDataAsync();
 
             Console.WriteLine($"LOCAL RESPONSE:\nCountry: {response.Country}\nPositive: {response.Positives}\nHealed: {response.Healed}\nDeceased: {response.Deceased}\nHospitalized: {response.Hospitalised}");
+
+            var intResponse = await api.GetInternationalDataAsync();
+
+            foreach (var data in intResponse)
+            {
+                Console.WriteLine(data.Attributes.Country);
+            }
+
             Console.ReadLine();
         }
     }
