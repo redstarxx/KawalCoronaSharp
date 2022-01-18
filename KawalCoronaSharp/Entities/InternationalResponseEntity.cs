09f83d3bfbf5d4dcd7da9d5ace87c16c76eeb72e
RedStar <mutazzmutazz11@gmail.com>
2022-01-18 10:24:19 +0700

    implement GetInternationalDataAsync

---
+++ KawalCoronaSharp/Entities/InternationalResponseEntity.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+
+namespace KawalCoronaSharp.Entities
+{
+    public class InternationalResponseEntityData
+    {
+        /// <summary>
+        /// Grts the object ID of the country.
+        /// </summary>
+        [JsonProperty("OBJECTID", NullValueHandling = NullValueHandling.Ignore)]
+        public int ObjectId { get; internal set; }
+
+        /// <summary>
+        /// Gets the name of the country / region.
+        /// </summary>
+        [JsonProperty("Country_Region", NullValueHandling = NullValueHandling.Ignore)]
+        public string Country { get; internal set; }
+
+        /// <summary>
+        /// Gets the last updated unix timestamp of the data.
+        /// </summary>
+        [JsonProperty("Last_Update", NullValueHandling = NullValueHandling.Ignore)]
+        public long LastUpdated { get; internal set; }
+
+        /// <summary>
+        /// Gets the latitude of the country.
+        /// </summary>
+        [JsonProperty("Lat", NullValueHandling = NullValueHandling.Ignore)]
+        public double? Latitude { get; internal set; }
+
+        /// <summary>
+        /// Gets the longitude of the country.
+        /// </summary>
+        [JsonProperty("Long_", NullValueHandling = NullValueHandling.Ignore)]
+        public double? Longitude { get; internal set; }
+
+        /// <summary>
+        /// Gets the number of confirmed cases of the country.
+        /// </summary>
+        [JsonProperty("Confirmed", NullValueHandling = NullValueHandling.Ignore)]
+        public int Confirmed { get; internal set; }
+
+        /// <summary>
+        /// Gets the number of the deaths of the country.
+        /// </summary>
+        [JsonProperty("Deaths", NullValueHandling = NullValueHandling.Ignore)]
+        public int Deaths { get; internal set; }
+
+        /// <summary>
+        /// Gets the number of the recovered patients of the country.
+        /// </summary>
+        [JsonProperty("Recovered", NullValueHandling = NullValueHandling.Ignore)]
+        public int Recovered { get; internal set; }
+
+        /// <summary>
+        /// Gets the number of the active cases of the country.
+        /// </summary>
+        [JsonProperty("Active", NullValueHandling = NullValueHandling.Ignore)]
+        public int Active { get; internal set; }
+    }
+
+    public class InternationalResponseEntity
+    {
+        [JsonProperty("attributes")]
+        public InternationalResponseEntityData Attributes { get; internal set; }
+    }
+}
--- KawalCoronaSharp/KawalCoronaApi.cs
+++ KawalCoronaSharp/KawalCoronaApi.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
@@ -32,6 +33,28 @@ namespace KawalCoronaSharp
             var deserializedResponse = JsonConvert.DeserializeObject<List<LocalResponseEntity>>(json);
 
             return deserializedResponse.First();
+        }
+
+        /// <summary>
+        /// Gets the international COVID statistics asynchronously.
+        /// </summary>
+        /// <returns>A <see cref="List{T}" /> of <see cref="InternationalResponseEntity" /> objects containing the statistics of each country.</returns>
+        public async Task<List<InternationalResponseEntity>> GetInternationalDataAsync()
+        {
+            string json = string.Empty;
+            string endpoint = $"{Endpoints.BASE_URL}";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endpoint);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                json = await reader.ReadToEndAsync();
+            }
+
+            return JsonConvert.DeserializeObject<List<InternationalResponseEntity>>(json);
         }
     }
 }
