cb4a30b406b6773e5df1a97f0622d7d479fcd295
RedStar <mutazzmutazz11@gmail.com>
2022-01-19 08:44:56 +0700

    implement separate global number endpoints

--- KawalCoronaSharp/Endpoints.cs
+++ KawalCoronaSharp/Endpoints.cs
@@ -5,5 +5,8 @@
         public const string BASE_URL = "https://api.kawalcorona.com";
         public const string LOCAL = "/indonesia/";
         public const string LOCAL_PROVINCES = "/indonesia/provinsi/";
+        public const string GLOBAL_POSITIVE = "/positif/";
+        public const string GLOBAL_RECOVERED = "/sembuh/";
+        public const string GLOBAL_DEATHS = "/meninggal/";
     }
 }
