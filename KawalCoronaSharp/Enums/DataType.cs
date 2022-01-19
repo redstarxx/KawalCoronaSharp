0953caad7010161448fdfe0b23a7e27ad600d129
RedStar <mutazzmutazz11@gmail.com>
2022-01-19 08:41:48 +0700

    add DataTypes

---
+++ KawalCoronaSharp/Enums/DataType.cs
@@ -0,0 +1,23 @@
+﻿namespace KawalCoronaSharp.Enums
+{
+    /// <summary>
+    /// The data type to get.
+    /// </summary>
+    public enum DataType
+    {
+        /// <summary>
+        /// Gets the global positive numbers.
+        /// </summary>
+        Positive,
+
+        /// <summary>
+        /// Gets the global recovered numbers.
+        /// </summary>
+        Recovered,
+
+        /// <summary>
+        /// Gets the global death numbers.
+        /// </summary>
+        Deaths
+    }
+}
