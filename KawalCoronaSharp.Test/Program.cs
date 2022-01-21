c6c2dbba453f12fe7f6ff29081f929e6315eb5e7
RedStar <mutazzmutazz11@gmail.com>
2022-01-21 10:08:28 +0700

    more more tests

--- KawalCoronaSharp.Test/Program.cs
+++ KawalCoronaSharp.Test/Program.cs
@@ -13,22 +13,24 @@ namespace KawalCoronaSharp.Test
 
             var response = await api.GetIndonesianDataAsync();
 
-            Console.WriteLine($"LOCAL RESPONSE:\nCountry: {response.Country}\nPositive: {response.Positives}\nHealed: {response.Recovered}\nDeceased: {response.Deceased}\nHospitalized: {response.Hospitalised}");
+            Console.WriteLine($"LOCAL RESPONSE:\nCountry: {response.Country}\nPositive: {response.Positives}\nRecovered: {response.Recovered}\nDeceased: {response.Deceased}\nHospitalized: {response.Hospitalised}");
 
-            var localResponse = await api.GetCountryDataAsync("rus", SearchMode.ClosestMatching);
+            var countryResponse = await api.GetCountryDataAsync("rus", SearchMode.ClosestMatching);
 
-            Console.WriteLine($"\n\nDeaths: {localResponse.Deaths}");
+            Console.WriteLine($"\nData for {countryResponse.Country} (GetCountryDataAsync)\nLast updated: {new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(countryResponse.LastUpdated)}\nConfirmed: {countryResponse.Confirmed}\nActive: {countryResponse.Active}\nRecovered: {countryResponse.Recovered}\nDeaths: {countryResponse.Deaths}\nLatitude: {countryResponse.Latitude}\nLongitude: {countryResponse.Longitude}\nObject ID: {countryResponse.ObjectId}\n");
 
-            var intResponse = await api.GetGlobalDataAsync();
+            var intResponse = await api.GetAllCountriesDataAsync();
 
             foreach (var data in intResponse)
             {
-                Console.WriteLine(data.LastUpdated);
+                Console.WriteLine($"\nData for {data.Country} (GetAllCountriesDataAsync)\nLast updated: {new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(data.LastUpdated)}\nConfirmed: {data.Confirmed}\nActive: {data.Active}\nRecovered: {data.Recovered}\nDeaths: {countryResponse.Deaths}\nLatitude: {data.Latitude}\nLongitude: {data.Longitude}\nObject ID: {data.ObjectId}\n");
             }
 
-            var globalResp = await api.GetPartialResponseDataAsync(DataType.Positive);
+            var globalPositive = await api.GetPartialGlobalDataAsync(DataType.Positive);
+            var globalRecovered = await api.GetPartialGlobalDataAsync(DataType.Recovered);
+            var globalDeaths = await api.GetPartialGlobalDataAsync(DataType.Deaths);
 
-            Console.WriteLine(globalResp.Value);
+            Console.WriteLine($"Global positives: {globalPositive.Value}\nGlobal recovered: {globalRecovered.Value}\nGlobal deaths: {globalDeaths.Value}");
 
             Console.ReadLine();
         }
